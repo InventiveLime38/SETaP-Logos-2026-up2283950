9e44dc0c1650b46cea3eb0e66120a2e6ca3482be
lemonizite <lemonizite@gmail.com>
2026-01-28 16:50:03 +0000

    implement turtle logo

--- Team5E.py
+++ Team5E.py
@@ -1,27 +1,63 @@
 import turtle
 #---~
+
+#setperameters
 t = turtle.Turtle()
+screen = turtle.Screen()
+screen.bgcolor("lightblue")
+screen.title("StudentSplit Logo Lem")
+screen.setup(width=330, height=330)
+len = 60
+step = len//20
+turnr = 90
+t.color("navyblue")
+t.width(5)
+#---~
 
-t.left(90)
+#drawoutercircle
+t.fillcolor("#FF8C00")
+t.begin_fill()
+t.penup()
+t.goto(0, -140)
+t.pendown()
+t.circle(140)
+t.end_fill()
+#---~
 
-t.forward(50)
-for i in range(180):
+#prepareforlogo
+t.penup()
+t.goto(-len//2, len//2)
+t.pendown()
+t.fillcolor("lightblue")
+t.begin_fill()
+#---~
+
+#createlogo
+t.left(turnr)
+for i in range(turnr * 3):
     t.right(1)
-    t.forward(1)
-t.forward(50)
-
-t.right(90)
-t.forward(60)
-t.left(90)
-
-t.forward(50)
-for i in range(180):
+    t.forward(1.1)
+t.right(turnr)
+t.forward(len)
+t.right(turnr)
+t.forward(step)
+t.backward(step)
+t.right(turnr)
+t.forward(len)
+for i in range(turnr * 3):
     t.right(1)
-    t.forward(1)
-t.forward(50)
+    t.forward(1.1)
+t.right(turnr)
+t.forward(len)
+t.right(turnr)
+t.forward(step)
+t.backward(step)
+t.right(turnr)
+t.forward(len)
+#---~
 
-t.right(90)
-t.forward(50)
-
-while True:
-    turtle.penup()
+#disappear
+t.end_fill()
+t.hideturtle()
+turtle.done()
+#---~
